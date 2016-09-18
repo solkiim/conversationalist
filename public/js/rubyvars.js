a8c6872e1c53d6648fee2ae5bdf252d0728d17ec
Sol Kim <solkim813@gmail.com>
2016-09-18 13:36:13 -0400

    delete unused files

--- public/js/rubyvars.js
+++
@@ -1,7 +0,0 @@
-//var name = <%=nameR%>;
-
-//window.onload = function() {
-//       //when the document is finished loading, replace everything
-//       //between the <a ...> </a> tags with the value of splitText
-//   document.getElementById("fill-bubble").innerHTML="PLS";
-//} 
