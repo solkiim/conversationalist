9837fe517ccff5a8334db19e6dbbf10bccca967e
ValerieMayTomic <valerie.tomic@gmail.com>
2016-09-18 01:13:17 -0400

    config

--- public/js/jsontest.js
+++
@@ -1,8 +0,0 @@
-$.ajax({
-    type: "POST",
-    url: "/sound",
-    data: { mydata: JSON.stringify(totalChanges) },
-    success: function(response, textStatus, jqXHR) {
-        console.log("Yay it worked.", response);
-    }
-);
