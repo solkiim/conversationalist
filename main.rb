1884f49dc1c35a766955059d0eb31c142fb442e0
ValerieMayTomic <valerie.tomic@gmail.com>
2016-09-18 00:36:14 -0400

    sinatra json etc

--- main.rb
+++ main.rb
@@ -1,6 +1,11 @@
 require 'rubygems'
 require 'sinatra'
+require 'json'
+#require "sinatra/json"
 #require 'sequel'
+
+current_volumes = {}
+talk_counts = Hash.new(0)
 
 get '/' do
 	redirect to('/index.html')
@@ -16,11 +21,23 @@ get '/room.html' do
 	send_file('/room.html')
 end
 
-post '/sound' do
-	data = JSON.parse request.body.read
-  	data['time']
+post '/data/userAndVolume' do
+	#puts "hey"
+	user = params['user']
+  	vol = params['volume']
+
+  	current_volumes[user, vol]
+  	max_val = current_volumes.values.max
+  	maximum_users = current_volumes.select {|k,v| v == max_val}
+
+  	maximum_users.each do |user,volume|
+  		talk_counts[user] += 1
+  	end
+
+  	json :talk_counts
 end
 
 get '/test' do
 	send_file('/test.html')
 end
+
--- public/js/room.js
+++ public/js/room.js
@@ -1,3 +1,61 @@
+
+/*
+ *  Copyright (c) 2015 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree.
+ */
+
+/* global AudioContext, SoundMeter */
+
+'use strict';
+
+var instantMeter = document.querySelector('#instant meter');
+
+try {
+  window.AudioContext = window.AudioContext || window.webkitAudioContext;
+  window.audioContext = new AudioContext();
+} catch (e) {
+  alert('Web Audio API not supported.');
+}
+
+// Put variables in global scope to make them available to the browser console.
+var constraints = window.constraints = {
+  audio: true,
+  video: false
+};
+
+function handleSuccess(stream) {
+  // Put variables in global scope to make them available to the
+  // browser console.
+  window.stream = stream;
+  var soundMeter = window.soundMeter = new SoundMeter(window.audioContext);
+  soundMeter.connectToSource(stream, function(e) {
+    if (e) {
+      alert(e);
+      return;
+    }
+    //console.log(soundMeter.instant.toFixed(2));
+    setInterval(function() {
+    //instantMeter.value should be the volume value, so you can return that at the end (probably)
+      instantMeter.value = soundMeter.instant.toFixed(2);  
+      sendData(instantMeter.value); 
+	  console.log(instantMeter.value);
+      //$("#vol").val(instantMeter.value)
+    }, 200);
+  });
+}
+
+function handleError(error) {
+  console.log('navigator.getUserMedia error: ', error);
+}
+
+
+navigator.mediaDevices.getUserMedia(constraints).
+    then(handleSuccess).catch(handleError);
+
+
 var displayedUsers = [];
 var currentUser = sessionStorage.getItem("name").replace(/\s+/, "").toLowerCase();
 
@@ -35,7 +93,7 @@ $(document).ready(function(){
 	um["nikitaramoji"] = 8;
 	changeTransparency(um);
 	
-	sendData([2,1,6,29]);
+	//sendData([2,1,6,29]);
 });
 
 function changeTransparency(usermap) {
